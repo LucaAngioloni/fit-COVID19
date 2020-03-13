89cf42abd2ddd0c53d6de573c40883140285d78a
Luca Angioloni <lucaangioloni@gmail.com>
2020-03-13 15:17:04 +0100

    cleaned code

--- fit.py
+++ fit.py
@@ -7,72 +7,50 @@ import os
 
 from scipy.optimize import curve_fit
 
-def sigmoid(x, L, k, x0, y0):
+days_past = -5
+days_future = 60
+
+def logistic(x, L, k, x0, y0):
     y = L / (1 + np.exp(-k*(x-x0))) + y0
     return y
 
-def sigmoid_derivative(x, L, k, x0):
+def logistic_derivative(x, L, k, x0):
     y = k * L * (np.exp(-k*(x-x0))) / np.power(1 + np.exp(-k*(x-x0)), 2)
     return y
 
-def fit_sigmoid(ydata, title, ylabel):
+def fit_logistic(ydata, title, ylabel):
     xdata = np.array(list(range(len(ydata)))) - len(ydata)
 
-    popt, pcov = curve_fit(sigmoid, xdata, ydata, bounds=([0, 0, -100, 0], [100000, 1, 100, 10]))
+    popt, pcov = curve_fit(logistic, xdata, ydata, bounds=([0, 0, -100, 0], [100000, 1, 100, 10]))
 
     print(title)
     print('    fit: L=%5.3f, k=%5.3f, x0=%5.3f, y0=%5.3f' % tuple(popt))
 
-    # plt.plot(xdata, ydata, 'b-', label='data')
-    # plt.plot(xdata, sigmoid(xdata, *popt), 'r-', label='fit: L=%5.3f, k=%5.3f, x0=%5.3f, y0=%5.3f' % tuple(popt))
-    # plt.xlabel('giorni dopo 19 febbraio')
-    # plt.ylabel(ylabel)
-    # plt.legend()
-    # plt.show()
-
-    # plt.figure(2)
-
-    total_xaxis = np.array(list(range(-5, 60))) - len(ydata)
-    plt.plot(total_xaxis, sigmoid(total_xaxis, *popt), 'g-', label='fit: L=%5.3f, k=%5.3f, x0=%5.3f, y0=%5.3f' % tuple(popt))
-    # y = np.zeros((50))
-    # y[:len(ydata)] = ydata
-    # plt.plot(list(range(50)), y, 'b-', label='data')
-    plt.plot(xdata, ydata, 'b-', label='data')
+    total_xaxis = np.array(list(range(days_past, days_future))) - len(ydata)
+    plt.plot(total_xaxis, logistic(total_xaxis, *popt), 'g-', label='prediction')
+    plt.plot(xdata, ydata, 'b-', label='real data')
     plt.xlabel('giorni (0 = oggi)')
     plt.ylabel(ylabel)
     plt.title(title + ' - ' + str(datetime.date.today().strftime("%d-%m-%Y")))
-    #plt.legend()
+    plt.legend()
     plt.show()
 
 
-def fit_sigmoid_derivative(ydata, title, ylabel):
+def fit_logistic_derivative(ydata, title, ylabel):
     xdata = np.array(list(range(len(ydata)))) - len(ydata)
 
-    popt, pcov = curve_fit(sigmoid_derivative, xdata, ydata, bounds=([0, 0, -100], [100000, 1, 100]))
+    popt, pcov = curve_fit(logistic_derivative, xdata, ydata, bounds=([0, 0, -100], [100000, 1, 100]))
 
     print(title)
     print('    fit: L=%5.3f, k=%5.3f, x0=%5.3f' % tuple(popt))
 
-    # plt.plot(xdata, ydata, 'b-', label='data')
-    # plt.plot(xdata, sigmoid(xdata, *popt), 'r-', label='fit: L=%5.3f, k=%5.3f, x0=%5.3f, y0=%5.3f' % tuple(popt))
-    # plt.xlabel('giorni dopo 19 febbraio')
-    # plt.ylabel(ylabel)
-    # plt.legend()
-    # plt.show()
-
-    # plt.figure(2)
-
-    total_xaxis = np.array(list(range(-5, 60))) - len(ydata)
-    plt.plot(total_xaxis, sigmoid_derivative(total_xaxis, *popt), 'g-',
-             label='fit: L=%5.3f, k=%5.3f, x0=%5.3f' % tuple(popt))
-    # y = np.zeros((50))
-    # y[:len(ydata)] = ydata
-    # plt.plot(list(range(50)), y, 'b-', label='data')
-    plt.plot(xdata, ydata, 'b-', label='data')
+    total_xaxis = np.array(list(range(days_past, days_future))) - len(ydata)
+    plt.plot(total_xaxis, logistic_derivative(total_xaxis, *popt), 'g-', label='prediction')
+    plt.plot(xdata, ydata, 'b-', label='real data')
     plt.xlabel('giorni (0 = oggi)')
     plt.ylabel(ylabel)
     plt.title(title + ' - ' + str(datetime.date.today().strftime("%d-%m-%Y")))
-    # plt.legend()
+    plt.legend()
     plt.show()
 
 if os.path.exists('data.csv'):
@@ -88,17 +66,17 @@ except:
 data = pd.read_csv('data.csv')
 
 ydata = data['totale_casi'].tolist()
-fit_sigmoid(ydata, 'Contagi', 'contagiati')
+fit_logistic(ydata, 'Contagi', 'contagiati')
 
 ydata = data['deceduti'].tolist()
-fit_sigmoid(ydata, 'Deceduti', 'deceduti')
+fit_logistic(ydata, 'Deceduti', 'deceduti')
 
 ydata = data['ricoverati_con_sintomi'].tolist()
-fit_sigmoid(ydata, 'Ricoverati', 'ricoverati')
+fit_logistic(ydata, 'Ricoverati', 'ricoverati')
 
 ydata = data['terapia_intensiva'].tolist()
-fit_sigmoid(ydata, 'Terapia Intensiva', 'in terapia')
+fit_logistic(ydata, 'Terapia Intensiva', 'in terapia')
 
 ydata = data['nuovi_attualmente_positivi'].tolist()
-fit_sigmoid_derivative(ydata, 'Nuovi positivi', 'nuovi positivi')
+fit_logistic_derivative(ydata, 'Nuovi positivi', 'nuovi positivi')
 
