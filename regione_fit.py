7ff6503a654a1c18605c9ecd2077237a6022142d
Luca Angioloni <lucaangioloni@gmail.com>
2020-03-19 16:27:24 +0100

    added region predictions

--- regione_fit.py
+++ regione_fit.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from io import StringIO
 from urllib import request as url_request
 import os
-import sys
+import sys    
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -23,107 +23,19 @@ myFmt = mdates.DateFormatter('%d/%m') # date formatter for matplotlib
 show_every = 3 # int value that defines how often to show a date in the x axis. (used not to clutter the axis)
 
 coeff_std = 1.2 # coefficient that defines how many standard deviations to use
-coeff_std_d = 0.5
+coeff_std_d = 0.01
 
-def logistic(x, L, k, x0, y0):
-    """
-    General Logistic function.
-
-    Args:
-        x    float or array-like, it represents the time
-        L    float, the curve's maximum value
-        k    float, the logistic growth rate or steepness of the curve.
-        x0   float, the x-value of the sigmoid's midpoint
-        y0   float, curve's shift in the y axis
-    """
-    y = L / (1 + np.exp(-k*(x-x0))) + y0
-    return y
-
-def logistic_derivative(x, L, k, x0):
-    """
-    General Gaussian like function (derivative of the logistic).
-
-    Args:
-        x    float or array-like, it represents the time
-        L    float, the curve's integral (area under the curve)
-        k    float, the logistic growth rate or steepness of the curve.
-        x0   float, the x-value of the max value
-    """
-    y = k * L * (np.exp(-k*(x-x0))) / np.power(1 + np.exp(-k*(x-x0)), 2)
-    return y
-
-def fit_curve(curve, ydata, title, ylabel, last_date, coeff_std, do_imgs=False):
-    xdata = np.array(list(range(-len(ydata), 0))) + 1
-
-    if curve.__name__ == 'logistic':
-        p0=[20000, 0.5, 1, 0]
-        bounds=([0, 0, -100, 0], [200000, 10, 100, 1])
-        params_names = ['L', 'k', 'x0', 'y0']
-    elif curve.__name__ == 'logistic_derivative':
-        p0=[20000, 0.5, 1]
-        bounds=([0, 0, -100], [200000, 10, 100])
-        params_names = ['L', 'k', 'x0']
-    else:
-        print('this curve is unknown')
-        return -1
-
-    popt, pcov = curve_fit(curve, xdata, ydata, p0=p0, bounds=bounds)
-
-    print(title)
-    descr = '    fit: '
-    for i, param in enumerate(params_names):
-        descr = descr + "{}={:.3f}".format(param, popt[i])
-        if i < len(params_names) - 1:
-            descr = descr + ', '
-    print(descr)
-
-    perr = np.sqrt(np.diag(pcov))
-    print(perr)
-
-    pworst = popt + coeff_std*perr
-    pbest = popt - coeff_std*perr
-
-    fig, ax = plt.subplots(figsize=(15,8))
-
-    ax.xaxis.set_major_formatter(myFmt)
-    fig.autofmt_xdate()
-
-    total_xaxis = np.array(list(range(-len(ydata) + days_past, days_future))) + 1
-
-    date_xdata = [last_date + timedelta(days=int(i)) for i in xdata]
-    date_total_xaxis = [last_date + timedelta(days=int(i)) for i in total_xaxis]
-
-    ax.plot(date_total_xaxis, curve(total_xaxis, *popt), 'g-', label='prediction')
-    ax.plot(date_xdata, ydata, 'b-', label='real data')
-
-    # popt, pcov = curve_fit(logistic, xdata[:-4], ydata[:-4], p0=[20000, 0.5, 1, 0], bounds=([0, 0, -100, 0], [200000, 10, 100, 1]))
-    # ax.plot(date_total_xaxis, logistic(total_xaxis, *popt), 'r-', label='old prediction')
-
-    future_axis = total_xaxis[len(ydata) - days_past:]
-    date_future_axis = [last_date + timedelta(days=int(i)) for i in future_axis]
-    ax.fill_between(date_future_axis, curve(future_axis, *pbest), curve(future_axis, *pworst), 
-        facecolor='red', alpha=0.2, label='std')
-
-    start = (len(ydata) - days_past - 1) % show_every
-    ax.set_xticks(date_total_xaxis[start::show_every])
-    ax.set_xlabel('Giorni - date')
-    ax.set_ylabel(ylabel)
-    ax.set_title(title + ' - ' + str(last_date.strftime("%d-%m-%Y")))
-    ax.legend(loc='upper left')
-    ax.grid(True)
-
-    if do_imgs:
-        plt.savefig('imgs/' + title + '.png', dpi=200)
-        plt.clf()
-    else:
-        plt.show()
-
-    return popt, perr
+from fit import logistic, logistic_derivative, fit_curve
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(
         description='Modello COVID-19 per regione.')
+    parser.add_argument(
+        '--regione',
+        type=str,
+        help='Nome regione su cui effettuare le predizioni.',
+        required=True)
     parser.add_argument(
         '--img',
         type=str,
@@ -142,11 +54,13 @@ if __name__ == '__main__':
         import matplotlib.pyplot as plt
         import matplotlib.dates as mdates
 
-    url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv'
+    url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv'
     webFile = url_request.urlopen(url).read()
     webFile = webFile.decode('utf-8')  
 
     data = pd.read_csv(StringIO(webFile))
+
+    data = data[data['denominazione_regione'] == args.regione]
 
     totale_casi = data['totale_casi'].tolist()
     deceduti = data['deceduti'].tolist()
